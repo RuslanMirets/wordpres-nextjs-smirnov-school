38e6a0ed8fb402c845b31074b1ea2ee226fdc861
Ruslan <ruslan.mirets@gmail.com>
2023-06-04 11:19:46 +0300

    Add axios

--- src/api/wp-api.ts
+++ src/api/wp-api.ts
@@ -1,16 +1,29 @@
-export const fetchData = async (query: string) => {
-	const headers = { "Content-Type": "application/json" };
+import axios from "axios";
 
-	const response = await fetch(`${process.env.WORDPRESS_API_URL}/graphql`, {
-		next: { revalidate: 60 },
-		headers,
+// export const fetchData = async (query: string) => {
+// 	const headers = { "Content-Type": "application/json" };
+
+// 	const response = await fetch(`${process.env.WORDPRESS_API_URL}/graphql`, {
+// 		next: { revalidate: 60 },
+// 		headers,
+// 		method: "POST",
+// 		body: JSON.stringify({
+// 			query,
+// 		}),
+// 	});
+
+// 	const json = await response.json();
+
+// 	return json.data;
+// };
+
+export const fetchData = async (query: string) => {
+	const response = await axios.request({
 		method: "POST",
-		body: JSON.stringify({
-			query,
-		}),
+		url: `${process.env.WORDPRESS_API_URL}/graphql`,
+		headers: { "Content-Type": "application/json" },
+		data: { query: query },
 	});
 
-	const json = await response.json();
-
-	return json.data;
+	return response.data.data;
 };
