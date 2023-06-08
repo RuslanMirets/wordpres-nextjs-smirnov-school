48a4622640c9c4322231b7f79f7f6466bd8451e2
Ruslan <ruslan.mirets@gmail.com>
2023-06-08 11:46:09 +0300

    Remove another middleware

--- next.config.js
+++ next.config.js
@@ -4,7 +4,6 @@ const nextConfig = {
 	env: {
 		WORDPRESS_API_URL: process.env.WORDPRESS_API_URL,
 		NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
-		NEXTAUTH_URL: process.env.NEXTAUTH_URL,
 		GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
 		GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
 	},
