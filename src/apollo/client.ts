1ae684af665ca531bdf427f09af0ab2e48a4c41e
Ruslan <ruslan.mirets@gmail.com>
2023-06-12 09:16:16 +0300

    Add new search

--- src/apollo/client.ts
+++ src/apollo/client.ts
@@ -2,10 +2,11 @@ import { ApolloClient, HttpLink, InMemoryCache } from "@apollo/client";
 
 const httpLink = new HttpLink({
 	uri: `${process.env.WORDPRESS_API_URL}/graphql`,
-	// credentials: "same-origin",
+	credentials: "same-origin",
 });
 
 const client = new ApolloClient({
+	ssrMode: true,
 	cache: new InMemoryCache(),
 	link: httpLink,
 	connectToDevTools: true,
