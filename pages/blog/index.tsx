e775bb42f039945998565f7476b212bd9ca33a01
Ruslan <ruslan.mirets@gmail.com>
2023-06-12 18:02:39 +0300

    Reset new cache

--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -1,33 +1,25 @@
-import { addApolloState, initializeApollo } from "@/src/apollo/apolloClient";
 import client from "@/src/apollo/client";
 import { PostApollo } from "@/src/apollo/post.apollo";
 import Blog from "@/src/screens/blog/Blog";
 import { IPostPreview } from "@/src/types/post.interface";
-import { useQuery } from "@apollo/client";
-import { GetStaticProps, GetStaticPropsContext } from "next";
+import { GetStaticProps } from "next";
 
-const BlogPage = () => {
-	const { data } = useQuery(PostApollo.GET_ALL, {
-		notifyOnNetworkStatusChange: true,
-	});
+interface IBlog {
+	posts: IPostPreview[];
+}
 
-	return <Blog posts={data.posts.nodes} />;
+const BlogPage = ({ posts }: IBlog) => {
+	return <Blog posts={posts} />;
 };
 
-export const getStaticProps: GetStaticProps = async (
-	context: GetStaticPropsContext,
-) => {
-	// const { data } = await client.query({ query: PostApollo.GET_ALL });
+export const getStaticProps: GetStaticProps = async () => {
+	const { data } = await client.query({ query: PostApollo.GET_ALL });
 
-	const apolloClient = initializeApollo();
-
-	await apolloClient.query({
-		query: PostApollo.GET_ALL,
-	});
-
-	return addApolloState(apolloClient, {
-		props: {},
-	});
+	return {
+		props: {
+			posts: data.posts.nodes,
+		},
+	};
 };
 
 export default BlogPage;
