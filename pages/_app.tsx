c11a0859a0010d5d6ce1bc617bbf0c9824cfa623
Ruslan <ruslan.mirets@gmail.com>
2023-06-13 12:33:41 +0300

    test

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,3 +1,4 @@
+import { useApollo } from "@/src/apollo/apolloClient";
 import client from "@/src/apollo/client";
 import Loader from "@/src/components/loader/Loader";
 import "@/styles/globals.scss";
@@ -6,8 +7,10 @@ import { SessionProvider } from "next-auth/react";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
+	const apolloClient = useApollo(pageProps);
+
 	return (
-		<ApolloProvider client={client}>
+		<ApolloProvider client={apolloClient}>
 			<SessionProvider>
 				<Loader />
 				<Component {...pageProps} />
--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -1,25 +1,30 @@
+import { addApolloState, initializeApollo } from "@/src/apollo/apolloClient";
 import client from "@/src/apollo/client";
 import { PostApollo } from "@/src/apollo/post.apollo";
 import Blog from "@/src/screens/blog/Blog";
 import { IPostPreview } from "@/src/types/post.interface";
 import { GetServerSideProps } from "next";
 
-type Props = {
-	posts: IPostPreview[];
-};
-
-const BlogPage = ({ posts }: Props) => {
-	return <Blog posts={posts} />;
-};
-
 export const getServerSideProps: GetServerSideProps = async () => {
-	const { data } = await client.query({ query: PostApollo.GET_ALL });
+	// const { data } = await client.query({ query: PostApollo.GET_ALL });
 
-	return {
-		props: {
-			posts: data.posts.nodes,
-		},
-	};
+	// return {
+	// 	props: {
+	// 		posts: data.posts.nodes,
+	// 	},
+	// };
+
+	const apolloClient = initializeApollo({});
+
+	await apolloClient.query({ query: PostApollo.GET_ALL });
+
+	return addApolloState(apolloClient, {
+		props: {},
+	});
+};
+
+const BlogPage = () => {
+	return <Blog />;
 };
 
 export default BlogPage;
