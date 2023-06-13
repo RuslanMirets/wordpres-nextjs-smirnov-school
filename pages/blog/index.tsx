019091a2ad9e80460d70d69a9d71d7deaf11bc1b
Ruslan <ruslan.mirets@gmail.com>
2023-06-13 13:03:29 +0300

    Reset

--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -1,30 +1,25 @@
-import { addApolloState, initializeApollo } from "@/src/apollo/apolloClient";
 import client from "@/src/apollo/client";
 import { PostApollo } from "@/src/apollo/post.apollo";
 import Blog from "@/src/screens/blog/Blog";
 import { IPostPreview } from "@/src/types/post.interface";
 import { GetServerSideProps } from "next";
 
-export const getServerSideProps: GetServerSideProps = async () => {
-	// const { data } = await client.query({ query: PostApollo.GET_ALL });
-
-	// return {
-	// 	props: {
-	// 		posts: data.posts.nodes,
-	// 	},
-	// };
-
-	const apolloClient = initializeApollo({});
-
-	await apolloClient.query({ query: PostApollo.GET_ALL });
-
-	return addApolloState(apolloClient, {
-		props: {},
-	});
+type Props = {
+	posts: IPostPreview[];
 };
 
-const BlogPage = () => {
-	return <Blog />;
+const BlogPage = ({ posts }: Props) => {
+	return <Blog posts={posts} />;
+};
+
+export const getServerSideProps: GetServerSideProps = async () => {
+	const { data } = await client.query({ query: PostApollo.GET_ALL });
+
+	return {
+		props: {
+			posts: data.posts.nodes,
+		},
+	};
 };
 
 export default BlogPage;
--- src/components/posts-list/PostsList.tsx
+++ src/components/posts-list/PostsList.tsx
@@ -2,16 +2,13 @@ import styles from "./PostsList.module.scss";
 import { IPostPreview } from "@/src/types/post.interface";
 import PostPreview from "../post-preview/PostPreview";
 import { useState } from "react";
-import { useQuery } from "@apollo/client";
-import { PostApollo } from "@/src/apollo/post.apollo";
 
-const PostsList = () => {
+type Props = {
+	posts: IPostPreview[];
+};
+
+const PostsList = ({ posts }: Props) => {
 	const [query, setQuery] = useState("");
-
-	const { data } = useQuery(PostApollo.GET_ALL);
-	const posts: IPostPreview[] = data.posts.nodes;
-
-	console.log(data);
 
 	return (
 		<div>
