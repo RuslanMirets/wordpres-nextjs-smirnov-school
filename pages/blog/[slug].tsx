b6f1f4fdcb1962ace9e116d1b3096eab23d27ec9
Ruslan <ruslan.mirets@gmail.com>
2023-06-14 16:39:12 +0300

    GetServerSideProps on dynamic pages

--- pages/blog/[slug].tsx
+++ pages/blog/[slug].tsx
@@ -1,25 +1,10 @@
 import client from "@/src/apollo/client";
 import { PostApollo } from "@/src/apollo/post.apollo";
 import Post from "@/src/screens/post/Post";
-import { IPostPreview, PostType } from "@/src/types/post.interface";
-import { GetStaticPaths, GetStaticProps } from "next";
+import { PostType } from "@/src/types/post.interface";
+import { GetServerSideProps } from "next";
 
-export const getStaticPaths: GetStaticPaths = async () => {
-	const { data } = await client.query({ query: PostApollo.GET_ALL });
-
-	const posts: IPostPreview[] = data.posts.nodes;
-
-	const paths = posts.map((post) => ({
-		params: { slug: post.slug },
-	}));
-
-	return {
-		paths,
-		fallback: false,
-	};
-};
-
-export const getStaticProps: GetStaticProps = async (context) => {
+export const getServerSideProps: GetServerSideProps = async (context) => {
 	const slug = context.params?.slug as string;
 
 	const { data } = await client.query({
