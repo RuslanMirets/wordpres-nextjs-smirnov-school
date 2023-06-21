3557a7405c423e573f69abd1a223d3813d7f601b
Ruslan <ruslan.mirets@gmail.com>
2023-06-21 15:06:13 +0300

    Fixes Product page

--- pages/products/index.tsx
+++ pages/products/index.tsx
@@ -1,21 +1,20 @@
-import client from "@/src/apollo/client";
+import { initializeApollo, addApolloState } from "@/src/apollo/apolloClient";
 import { ProductApollo } from "@/src/apollo/product.apollo";
 import Products from "@/src/screens/products/Products";
-import { ProductsType } from "@/src/types/product.interface";
 import { GetServerSideProps } from "next";
 
-export const getServerSideProps: GetServerSideProps = async () => {
-	const { data } = await client.query({ query: ProductApollo.GET_ALL });
-
-	return {
-		props: {
-			products: data.products.nodes,
-		},
-	};
+const ProductsPage = () => {
+	return <Products />;
 };
 
-const ProductsPage = ({ products }: ProductsType) => {
-	return <Products products={products} />;
+export const getServerSideProps: GetServerSideProps = async () => {
+	const apolloClient = initializeApollo();
+
+	await apolloClient.query({ query: ProductApollo.GET_ALL });
+
+	return addApolloState(apolloClient, {
+		props: {},
+	});
 };
 
 export default ProductsPage;
--- src/screens/products/Products.tsx
+++ src/screens/products/Products.tsx
@@ -1,12 +1,20 @@
+import { ProductApollo } from "@/src/apollo/product.apollo";
 import Layout from "@/src/components/Layout";
 import ProductCard from "@/src/components/product-card/ProductCard";
-import { ProductsType } from "@/src/types/product.interface";
+import { IProducts, ProductsType } from "@/src/types/product.interface";
 import Container from "@/src/ui/container/Container";
+import Heading from "@/src/ui/heading/Heading";
+import { useQuery } from "@apollo/client";
 
-const Products = ({ products }: ProductsType) => {
+const Products = () => {
+	const { data } = useQuery(ProductApollo.GET_ALL);
+
+	const products: IProducts[] = data?.products.nodes;
+
 	return (
 		<Layout title="Товары">
 			<Container>
+				<Heading>Товары</Heading>
 				<ul className="mb-10 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4 max-w-xs mx-auto sm:max-w-none">
 					{products.map((product) => (
 						<li key={product.databaseId}>
